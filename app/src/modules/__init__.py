a5398b098f01c13390e0e23bd3926bdc65645283
Anthony <a.sauvage@socofer.com>
2025-01-06 09:44:13 +0100

    ASA - MAJ upload

---
+++ app/src/modules/__init__.py
@@ -0,0 +1,6 @@
+"""
+Package modules contenant les modules fonctionnels de l'application.
+
+Ce package contient les différents modules qui implémentent
+les fonctionnalités spécifiques de l'application.
+"""
